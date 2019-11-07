5b22996ee6a8e661dac86c1629aae80d3e958ecd
vyagi <marcin.jagiela@hotmail.com>
2019-11-07 17:40:24 +0100

    Initial work done

---
+++ ClassesNo3/TestGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Priwiet Marcin");
+        }
+    }
+}
