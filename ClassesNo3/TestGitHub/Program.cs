4602a84d579553c36d548e33c0a64feec7f6fa53
vyagi <marcin.jagiela@hotmail.com>
2019-11-07 17:51:53 +0100

    New line added

--- ClassesNo3/TestGitHub/Program.cs
+++ ClassesNo3/TestGitHub/Program.cs
@@ -7,6 +7,7 @@ namespace TestGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Priwiet Marcin");
+            Console.WriteLine("How is your day today");
         }
     }
 }
