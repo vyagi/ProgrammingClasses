022dee02970f17ca55c083ed6044857b10feefd1
vyagi <marcin.jagiela@hotmail.com>
2019-11-07 18:44:04 +0100

    MathUtilities class created with one method

---
+++ ClassesNo3/BasicMethods/MathUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicMethods
+{
+    public static class MathUtilities
+    {
+        public static double SolveLinearEquation(double a, double b)
+        {
+            return a == 0 ? 0 : -b / a;
+            //if(a == 0)
+            //    return 0;
+            
+            //return -b / a;
+        }
+    }
+}
---
+++ ClassesNo3/BasicMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathUtilities.SolveLinearEquation(3, 6));
+        }
+    }
+}
