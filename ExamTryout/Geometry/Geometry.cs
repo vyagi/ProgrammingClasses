2d6bbbc427c40c5b693adbea7143bc16fa565b62
vyagi <marcin.jagiela@hotmail.c>
2020-01-30 18:16:10 +0100

    Task no 2

---
+++ ExamTryout/Geometry/Geometry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry
+{
+    class Geometry
+    {
+        private double _x;
+        private double _y;
+
+        public double X => _x;
+        public double Y => _y;
+        public Geometry()
+        {
+            _x = 0;
+            _y = 0;
+        }
+        public Geometry(double z)
+        {
+            _x = z;
+            _y = z;
+        }
+        public Geometry(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+    }
+}
